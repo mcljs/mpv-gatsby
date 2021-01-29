4b33928f6edb0c394e70ae941a0c9dce0732a01c
Michael Chacon <mcljs15@gmail.com>
2021-01-29 15:07:25 -0400

    Cambio de SEO Header

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -3,25 +3,19 @@ import './HeroSection.css';
 import { FaTwitter} from 'react-icons/fa'
 import {Button} from '../ButtonElements'
 import {SocialIconLink} from '../Footer/FooterElements'
-import Link from '../link';
-import config from '../../../config/website';
 
 function HeroSection(){
-
-   let url_twitter = `${config.twitter}`
     return(
         <div className="hero-container">
             <h1>Movimiento Por la Paz y la Vida</h1>
          
             <div className="hero-btns">
-                <Link to={url_twitter}>
-                    <Button fontBig big primary>
-            <SocialIconLink rel="twitter" style={{textDecoration: 'none',color: '#fff',fontFamily: 'sans-serif'}}>
+            <Button fontBig big primary>
+            <SocialIconLink href="https://twitter.com/DPazYVida3" target="_blank" rel="twitter" style={{textDecoration: 'none',color: '#fff',fontFamily: 'sans-serif'}}>
         SIGUENOS @
       </SocialIconLink>
                  <FaTwitter style={{textAlign: 'right'}}></FaTwitter>
             </Button>
-                </Link>
             </div>
         </div>
     )
