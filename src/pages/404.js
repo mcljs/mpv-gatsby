ae00b787f668736aacefa81c865768380b42307c
Michael <mcljs15@gmail.com>
2021-02-01 15:00:41 -0400

    cambios letras reponsive gran base

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import SearchForm from "../components/search-form"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Oops Te equivocaste</h1>
+    <h1>404:  Oops Te equivocaste</h1>
     <p>Regresa al inicio</p>
     <SearchForm/>
   </Layout>
