732a5ae17136f3bb89c553c660786df0aeba94c8
Michael <mcljs15@gmail.com>
2021-02-19 16:39:54 -0400

    contraste bottom del header

--- src/components/ButtonElements.js
+++ src/components/ButtonElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Button = styled.button`
     border-radius: 4px;
-    background: ${({primary}) =>(primary ? "rgb(250,56,94)" : "rgb(226,52,86)")};
+    background: ${({primary}) =>(primary ? "#C81617" : "#E2191A")};
     white-space: nowrap;
     padding: ${({big}) => (big ? "20px 64px" : "10px 20px")};
     color: #fff;
@@ -14,7 +14,7 @@ export const Button = styled.button`
 
     &:hover {
         transition: all 0.3s ease-out;
-        background: ${({primary}) =>(primary ? "rgb(226,52,86)" :  "rgb(250,56,94)")};
+        background: ${({primary}) =>(primary ? "#E2191A" :  "#C81617")};
     } 
 
     @media screen and (max-width: 960px){
