239ccc4476eb84795bdc00c02edea5e1ba5deaa8
Michael Chacon <mcljs15@gmail.com>
2021-02-27 01:19:31 -0400

    fix SEO biblioteca

---
+++ src/pages/biblioteca/general.js
@@ -0,0 +1,3 @@
+import React from 'react'
+import SEO from '../../components/Seo';
+export const general = () => ( <SEO title="Biblioteca Digital"/> )
---
+++ src/pages/biblioteca/plan-89.js
@@ -0,0 +1,3 @@
+import React from 'react'
+import SEO from '../../components/Seo';
+export const plan89 = () => ( <SEO title="PLAN 89"/> )
