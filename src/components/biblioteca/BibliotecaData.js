755afff5f5e33cf645e73f9d159760b30b254f82
Michael Chacon <mcljs15@gmail.com>
2021-02-26 23:43:46 -0400

    fix bibiblioteca planes y programas

--- src/components/biblioteca/BibliotecaData.js
+++ src/components/biblioteca/BibliotecaData.js
@@ -1,4 +1,3 @@
-import ImageOne from '../../bibliotecaImage/libro-azul.jpg'
 import Plan89 from '../../bibliotecaImage/plan89.jpg'
 
 export const BibliotecaDataPlanes = [
@@ -16,18 +15,5 @@ export const BibliotecaDataPlanes = [
     number: 2,
     image: Plan89
   },
-  {
-    id: 'libro-azul',
-    title: 'El Libro Azul',
-    publisher: 'Planes y Programas',
-    genero: 'Biografica',
-    fecha_p: '2007',
-    n_paginas: '127',
-    encuadernacion: 'Tapa blanda',
-    autor: 'Hugo Chavez',
-    description: 'El libro azul es un libro de mandatos y profecías, la raíz primigenia del Proyecto Bolivariano que está en desarrollo en Venezuela. Todo patriota venezolano debe conocer este libro, aquí está proyectado el futuro del siglo XXI", dice en el prólogo Nicolás Maduro. Un libro fundamental para entender el movimiento chavista desde el proyecto del propio Chávez.',
-    descarga: 'http://www.minpi.gob.ve/assets/pdf/arc_Libro_Azul.pdf',
-    number: 1,
-    image: ImageOne 
-  },
+
 ]
