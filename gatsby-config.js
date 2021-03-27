76a51a9907543c0dac24297087c10e6f1cd41a41
Michael <mcljs15@gmail.com>
2021-03-27 00:39:31 -0400

    Remove SW

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+  `gatsby-plugin-remove-serviceworker`,
     `gatsby-plugin-netlify-cms`,
   ],
 }
